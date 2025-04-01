1e2c1703383918d62f0102d151353cfb12e11924
Brayden Humpherys <21brayden.rich@gmail.com>
2025-04-01 00:46:52 -0600

    update RFOversampler to handle categorical

--- rfoversample.py
+++ rfoversample.py
@@ -3,27 +3,81 @@ import pandas as pd
 from rfgap import RFGAP
 
 
-class RFOversample:
+class RFOversampler:
+    # """
+    # Currently (01-28-25) wrtten to upsample all minority classes in a dataset to the number of observations
+    # of the majority class. 
+    # """
+    # def __init__(self, dataframe, target_column, num_samples=3):
+    #     """
+    #     dataframe: A pandas dataframe that containes the features and target
+    #     target_column: Target column name as a string or target column index as an integer
+    #     num_samples: Default to 3. During the oversampling process, the num_samples will determine the
+    #         number of points used to generate new feature values for a new point
+    #     """
+    #     if isinstance(target_column, str):
+    #         self.y = dataframe[target_column]
+    #         self.x = dataframe.drop([target_column], axis = 1)
+    #     elif isinstance(target_column, int):
+    #         self.y = dataframe.iloc[:,target_column]
+    #         self.x = dataframe.drop(dataframe.columns[target_column], axis = 1)
+    #     self.num_samples = num_samples
+    #     self.rf = RFGAP(y = self.y, prediction_type = "classification", matrix_type = 'dense', class_weight = 'balanced')
+
+
+    # def fit(self):
+        # """
+        # This method will iterate through each minority class and artifically create observations with new
+        # feature values using RF-proximities as weights. This method will return a tuple of:
+        #     1st: A pandas dataframe (x) containing all features and their values (including the new data points)
+        #     2nd: A padnas series (y) containing all target values (including the new data points) corresponding
+        #         to the correct index in x.
+        # """
+    #     self.rf.fit(self.x, self.y)
+    #     prox = self.rf.get_proximities()
+
+    #     value_counts = self.y.value_counts()
+    #     maj_label = value_counts.index[0]
+    #     maj_count = value_counts.iloc[0]
+    #     class_counts = value_counts.to_dict()
+
+    #     num_features = self.x.shape[1]
+
+    #     for label in class_counts:
+    #         if label != maj_label:
+    #             upsample_size = maj_count - class_counts[label]
+    #             new_points = np.zeros((upsample_size, num_features))
+
+    #             for i in range(upsample_size):
+    #                 sample_indices = np.where(self.y == label)[0]
+    #                 samples = np.random.choice(sample_indices, self.num_samples, replace=True)
+    #                 new_features = np.sum((np.matmul(np.transpose(self.x.iloc[samples, :]), prox[samples, :])), axis=1) / self.num_samples
+    #                 new_points[i, :] = new_features
+
+    #             self.x = pd.concat((self.x, pd.DataFrame(new_points, columns=self.x.columns))).reset_index(drop=True)
+    #             self.y = pd.concat((self.y, pd.Series(np.ones_like(new_points[:, 0]) * label))).reset_index(drop=True)
+
+    #     return self.x, self.y
+
     """
-    Currently (01-28-25) wrtten to upsample all minority classes in a dataset to the number of observations
-    of the majority class. 
+    Currently (04-01-25) wrtten to upsample all minority classes in a dataset to the number of observations of the majority class.
     """
-    def __init__(self, dataframe, target_column, num_samples=3):
+    def __init__(self, Data, target_ind=0, num_samples=3, contains_categoricals=False, encoded=False, cat_cols=None):
         """
         dataframe: A pandas dataframe that containes the features and target
         target_column: Target column name as a string or target column index as an integer
         num_samples: Default to 3. During the oversampling process, the num_samples will determine the
             number of points used to generate new feature values for a new point
+        contains_categoricals: Default to False. If the dataset contains categorical features, set to True
+        encoded: Default to False. If the dataset contains dummy encoded categorical features, set to True
+        cat_cols: Default to None. If the dataset contains categorical features, provide a list of the column indices
         """
-        if isinstance(target_column, str):
-            self.y = dataframe[target_column]
-            self.x = dataframe.drop([target_column], axis = 1)
-        elif isinstance(target_column, int):
-            self.y = dataframe.iloc[:,target_column]
-            self.x = dataframe.drop(dataframe.columns[target_column], axis = 1)
+        self.Data = Data
+        self.target_ind = target_ind
         self.num_samples = num_samples
-        self.rf = RFGAP(y = self.y, prediction_type = "classification", matrix_type = 'dense', class_weight = 'balanced')
-
+        self.contains_categoricals = contains_categoricals
+        self.encoded = encoded
+        self.cat_cols = cat_cols
 
     def fit(self):
         """
@@ -33,30 +87,118 @@ class RFOversample:
             2nd: A padnas series (y) containing all target values (including the new data points) corresponding
                 to the correct index in x.
         """
-        self.rf.fit(self.x, self.y)
-        prox = self.rf.get_proximities()
 
-        value_counts = self.y.value_counts()
+        numof_cat_cols = 0
+
+        #make a dictionary with each categorical column and its unique values. This will be useful further down the line
+        cat_dict = {}
+
+        if not self.encoded:
+            # If NOT encoded, store unique values for each categorical column
+            for col in self.cat_cols:
+                cat_dict[col] = self.Data[col].nunique()
+                numof_cat_cols = len(self.cat_cols)
+        else:
+            # If encoded, infer categorical groups from one-hot encoded column names
+            encoded_groups = {}
+            for col in self.Data.columns:
+                for cat_col in self.cat_cols:
+                    if col.startswith(cat_col + "_"):  # Check if the column belongs to a one-hot encoded group
+                        encoded_groups[cat_col] = encoded_groups.get(cat_col, 0) + 1
+                        numof_cat_cols += 1
+
+            cat_dict = encoded_groups  # Store the grouped one-hot column names
+            numof_num_cols = len(self.Data.columns) - numof_cat_cols - 1 #calculate number of numerical columns (-1 for target variable)
+
+        x = None
+        y = None
+
+        if not self.encoded:    
+            data_encoded = pd.get_dummies(self.Data, columns=self.cat_cols, dtype=int) #encode the categorical columns
+            y = data_encoded.iloc[:, self.target_ind]
+            x = data_encoded.drop(self.Data.columns[self.target_ind], axis=1)
+        else:
+            y = self.Data.iloc[:, self.target_ind]
+            x = self.Data.drop(self.Data.columns[self.target_ind], axis=1)
+
+        #Train RF, get proximities
+        rf = RFGAP(y = y, prediction_type = 'classification', matrix_type = 'dense')
+        rf.fit(x, y)
+        prox = rf.get_proximities()
+
+        #Get the Majority Class, its count, and store all classes and their counts in a dictionary. This will be useful further down the line
+        value_counts = y.value_counts()
         maj_label = value_counts.index[0]
         maj_count = value_counts.iloc[0]
         class_counts = value_counts.to_dict()
 
-        num_features = self.x.shape[1]
+        # Separate numerical and categorical data
+        encoded_cat_cols = x.columns[numof_num_cols:]
+        x_numerical = x.drop(columns=encoded_cat_cols)
+        x_categorical = x[encoded_cat_cols] 
 
+        #store number of features in each type
+        num_features_size = x_numerical.shape[1]
+        cat_features_size = x_categorical.shape[1]
+
+        #Loop through all classes that aren't majority class and upsample
         for label in class_counts:
             if label != maj_label:
+
+                #Get number of samples to upsample
+                #initialize arrays to store new numerical and categorical features
                 upsample_size = maj_count - class_counts[label]
-                new_points = np.zeros((upsample_size, num_features))
+                new_points_num = np.zeros((upsample_size, num_features_size))
+                new_points_cat = np.zeros((upsample_size, cat_features_size), dtype=int)
 
+                #generate new samples until the number of samples is equal to the majority class
                 for i in range(upsample_size):
-                    sample_indices = np.where(self.y == label)[0]
+
+                    #sample random points from the class that needs to be upsampled
+                    sample_indices = np.where(y == label)[0]
                     samples = np.random.choice(sample_indices, self.num_samples, replace=True)
-                    new_features = np.sum((np.matmul(np.transpose(self.x.iloc[samples, :]), prox[samples, :])), axis=1) / self.num_samples
-                    new_points[i, :] = new_features
 
-                self.x = pd.concat((self.x, pd.DataFrame(new_points, columns=self.x.columns))).reset_index(drop=True)
-                self.y = pd.concat((self.y, pd.Series(np.ones_like(new_points[:, 0]) * label))).reset_index(drop=True)
+                    #Get new numerical features and add to new points array
+                    new_features_num = np.sum((x_numerical.T.iloc[:, samples] @ prox[samples, :]), axis=1)/self.num_samples #proximity weighted average of the samples
+                    new_points_num[i, : ] = new_features_num
 
-        return self.x, self.y
+                    #Get new categorical features and add to new points array
+                    start_x_slice_ind = 0 #variable to help in slicing categorical columns to focus on one encoded categorical feature at a time
+                    for categorical_feature in cat_dict: #loop through the features in the encoded x_categorical columns
+
+                        numof_values_in_feature = cat_dict[categorical_feature] #number of columns in x_categorical dedicated to specified encoded feature - used in slicing
+                        end_x_slice_ind = numof_values_in_feature + start_x_slice_ind
+                        this_encoded_feature = x_categorical.iloc[:, start_x_slice_ind:end_x_slice_ind] #slice to focus on the encoded columns of a feature, one feature at a time
+
+                        new_feature_cat_ind = np.sum((this_encoded_feature.T.iloc[:, samples] @ prox[samples, :]).T, axis=0).argmax(axis=0) #proximity weighted 'best guess' of the samples
+
+                        #loop through the columns of the encoded feature and set the column with the highest proximity to 1 and the rest to 0
+                        for j in range(numof_values_in_feature): 
+                            if j == new_feature_cat_ind:
+                                    new_points_cat[i, j + start_x_slice_ind] = 1
+                            else:
+                                    new_points_cat[i, j + start_x_slice_ind] = 0
+                        start_x_slice_ind = end_x_slice_ind #update starting index for slicing
+
+                #concatenate the new numerical and categorical features to the original data
+                if not self.encoded:
+                    new_points_cat_non_dummy = pd.from_dummies(pd.DataFrame(new_points_cat, columns=x_categorical.columns), sep='_')
+                    new_combined_x = pd.concat((pd.DataFrame(new_points_num, columns=x_numerical.columns), 
+                                                new_points_cat_non_dummy), axis=1)
+
+                    old_points_cat_non_dummy = pd.from_dummies(x_categorical, sep='_')
+                    old_combined_x = pd.concat((x_numerical, old_points_cat_non_dummy), axis=1)
+                else:
+                    new_combined_x = pd.concat((pd.DataFrame(new_points_num, columns=x_numerical.columns), 
+                                                pd.DataFrame(new_points_cat, columns=x_categorical.columns)), axis=1)
+                    old_combined_x = pd.concat((x_numerical, 
+                                                x_categorical), axis=1)
+                x = pd.concat((old_combined_x, 
+                            new_combined_x), axis=0).reset_index(drop=True)
+                y = pd.concat((y, 
+                            pd.Series(np.ones_like(new_points_num[:, 0]) * label, 
+                                        dtype=int))).reset_index(drop=True)
+
+        return x, y
 
 
