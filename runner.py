3d50ae62d278217f2f5cd86bed84e82311fb5b46
Brayden Humpherys <21brayden.rich@gmail.com>
2025-04-16 11:45:17 -0600

    fix rfoversample on numeric

--- runner.py
+++ runner.py
@@ -3,15 +3,11 @@ from SupportFunctions.cross_val import run_experiment
 if __name__ == "__main__":
     config = {
         "dataset_folder": "datasets",
-        "selected_datasets": ["diabetes"],
+        "selected_datasets": ["hill_valley"],
         "methods": ["rfoversample"],
         # Model compatability: "adasyn", "random_undersampling" (if applicable)
         "imbalance_ratios": [0.2],
-<<<<<<< HEAD
-        "encoding_methods": ["onehot"], # or ordinal
-=======
         "encoding_methods": ["other"], # or ordinal
->>>>>>> 1c5862261008c1288b97717ad7b674e7bcfa003f
         "use_archetypes": [False],
         "archetype_settings": [{"archetype_proportion": 0.2}],
         "minority_sample_settings": [{"sample_percentage": 0.5}],
