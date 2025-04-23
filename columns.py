fec18e2bb4f36d5702cfe5be88fac316b9cc1c87
james klein <rjamesklein13@gmail.com>
2025-04-22 21:41:43 -0600

    columns.py

---
+++ columns.py
@@ -0,0 +1,12 @@
+categorical_cols = {
+    'artificial_tree': None,
+    'audiology': 'all',
+    'balance_scale': None,
+    'breast_cancer': None,
+    'car': 'all',
+    'chess': 'all',
+    'crx': [0, 1, 3, 4, 5, 6, 8, 9, 11, 12],
+    'diabetes': None,
+    'ecoli_5': None,
+    'flare1': [0, 1, 2]
+}
