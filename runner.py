49fca82e8462c61176298858c0f2b2ad49727249
James Klein <rjamesklein13@gmail.com>
2025-06-20 16:05:31 -0400

    updated runner

--- runner.py
+++ runner.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
         "selected_datasets": ["titanic", "sonar", "diabetes", "heart_disease", "heart_failure", "ionosphere", "parkinsons"],
         "methods": ['none', "smote", "class_weights", "adasyn", "random_undersampling", "rfoversample", "baseline"], #none, class_weights, adasyn, random_undersampling, rfoversample, smotenc
         "encoding_methods": ["ordinal"],
-        "imbalance_ratios": [0.15, 0.2, 0.25],
+        "imbalance_ratios": [0.15, 0.2, 0.25, 0.3, 0.35],
 
         # Archetypes
-        "use_archetypes": [False],
+        "use_archetypes": [True, False],
         "archetype_settings": [{"archetype_proportion": 0.2}],
         "minority_sample_settings":[{"sample_percentage": 0.5}],
 
         # Cross-validation using pre-generated seeds
         "random_states": random_seeds,
-        "n_folds": 3,
+        "n_folds": 5,
         "n_jobs": -1,
 
         # Save Results
